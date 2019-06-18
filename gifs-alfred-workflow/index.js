4403b46281d3b6fd0c0316babff810cadd92155a
Willson Smith <me@willsonsmith.com>
2019-06-17 20:03:03 -0400

    show all if no argument

--- gifs-alfred-workflow/index.js
+++ gifs-alfred-workflow/index.js
@@ -6,15 +6,19 @@ function searchGifs(searchTerm, list) {
   return list.filter((item) => item.tags.some((tag) => tag.includes(searchTerm)));
 }
 
+function createObject(item) {
+  return {title: item.name, subtitle: '', arg: `${baseUrl}/gifs/${item.name}`}
+}
+
 (async () => {
   const list = await alfy.fetch(`${baseUrl}/gifs.json`);
   
-  
-  const items = searchGifs(alfy.input, list).map(
-    (item, index) => (
-      {title: item.name, subtitle: '', arg: `${baseUrl}/gifs/${item.name}`}
-    )
-  );
+  if (!alfy.input) {
+    alfy.output(list.map(createObject))
 
-  alfy.output(items);
+  } else {
+    const items = searchGifs(alfy.input, list).map(createObject);
+
+    alfy.output(items);
+  }
 })();
